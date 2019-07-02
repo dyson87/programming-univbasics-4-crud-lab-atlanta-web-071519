f59e0216d43f87d2919a034dbdf77b0e2e452677
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-02 23:55:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.unshift(create_an_array,"Lauren")
+  
 end
 
 def add_element_to_start_of_array(array, element)
